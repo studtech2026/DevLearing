05fd1d75481a2c1202bf675278c3290e81ff8aeb
Rajmaheta009 <rajmaheta2008@gmail.com>
2026-07-19 20:16:28 +0530

    all is compalte

--- DEvLerner-backend/DEvLerner-backend/app/db/database.py
+++
@@ -1,8 +0,0 @@
-import psycopg2
-from psycopg2.extras import RealDictCursor
-from app.core.config import DATABASE_URL
-
-
-def get_db_connection():
-    conn = psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
-    return conn
--- DEvLerner-backend/DEvLerner-backend/main.py
+++
@@ -1,7 +0,0 @@
-from app.main import app
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
